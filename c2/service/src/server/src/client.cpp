df434d179a33b04eabf7dca1773d82e0ac1e1fc2
JquarkLegion <cjq1822261285@outlook.com>
2024-10-13 22:47:15 +0800

    c2serviceclient

--- c2/service/src/server/src/client.cpp
+++ c2/service/src/server/src/client.cpp
@@ -12,7 +12,11 @@
     参数的动态提交：    
     1.格式：rosrun 包名 节点名 参数值1 参数值2
     2.节点执行时，会自动读取命令中的参数，并组织进request中 
+
+    先启动客户端，再启动服务端时，客户端会等待服务端启动：
+    ros中内置函数，客户端挂起等待，直到服务端启动
 */
+
 int main(int argc, char *argv[])
 {
     if(argc != 3)
@@ -28,9 +32,13 @@ int main(int argc, char *argv[])
     // 4.创建一个客户端对象
     ros::ServiceClient client = nh.serviceClient<server::addInt>("addInt");
     // 5.提交请求并处理响应
+    // 5.1组织请求  
     server::addInt ai;
     ai.request.a = atoi(argv[1]); // 提交数据
     ai.request.b = atoi(argv[2]); // 提交数据
+    // 5.2发送请求
+    // client.waitForExistence(); // 等待服务端启动   
+    ros::service::waitForService("addInt"); // 等待服务端启动
     bool flag = client.call(ai); // 发送请求
     if (flag)
     {
