71b67183dde3879a452513a62c902f2a0eb248cd
JquarkLegion <cjq1822261285@outlook.com>
2024-10-16 22:59:38 +0800

    c6Oct16th

---
+++ c6/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/jquark/ROS/c6/devel:$CMAKE_PREFIX_PATH"
+export PATH='/opt/ros/noetic/bin:/home/jquark/.local/bin:/home/jquark/.local/bin:/tmp/.mount_cursorOSBAUX:/tmp/.mount_cursorOSBAUX/usr/sbin:/home/jquark/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/snap/bin'
+export PWD='/home/jquark/ROS/c6/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/jquark/ROS/c6/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/jquark/ROS/c6/src:$ROS_PACKAGE_PATH"
---
+++ c6/build/googletest/CMakeFiles/gtest.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "/home/jquark/ROS/c6/build/gmock/lib/libgtest.a"
+)
